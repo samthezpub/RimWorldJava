694c7ad4cad2ebe733d1754de9d6bef8a3fd54f9
Владимир Миргородский <mirgorodscky.volodymyr@yandex.ru>
2025-02-17 23:33:31 +0300

    hashcode & equals character

--- src/entity/CharacterContext.java
+++ src/entity/CharacterContext.java
@@ -4,6 +4,8 @@ import builder.CharacterBuilder;
 import enums.CharacterTraits;
 import interfaces.CharacterState;
 
+import java.util.Arrays;
+
 public class CharacterContext {
     private String name;
     private int age;
@@ -40,5 +42,35 @@ public class CharacterContext {
     }
     public void setState(CharacterState state) {
         this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return false;
+        if (o == null || this.getClass() != o.getClass()){
+            return false;
+        }
+
+        CharacterContext character = (CharacterContext) o;
+        if (character.name.equals(character.name)
+                && this.age == character.age
+                && this.traits.equals(character.traits)
+                && this.state.equals(character.state))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        int result = 31;
+        result = result + 31 * name.hashCode();
+        result = result + 31 * age;
+        result = result + 31 * Arrays.hashCode(traits);
+        result = result + 31 * state.hashCode();
+
+        return result;
     }
 }
